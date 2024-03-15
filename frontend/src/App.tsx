a9a24c7ecc2712ab225a7a1b6fb6b0357c5de1a1
SJ-Kwak <seojin7@ewhain.net>
2024-03-15 19:11:48 +0900

    feat: kakao map 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Map from "./components/map/Map";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>지도</h1>
+      <Map />
     </div>
   );
 }
---
+++ frontend/src/components/map/Map.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+
+export default function Map() {
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(37.5635, 126.9467), // 지도의 중심좌표
+          level: 4, // 지도의 확대 레벨
+        };
+        new window.kakao.maps.Map(mapContainer, mapOption);
+      });
+    };
+    mapScript.addEventListener('load', onLoadKakaoMap);
+  }, []);
+
+  return (
+    <div
+      id="map"
+      style={{
+        width: "500px",
+        height: "400px",
+      }}
+    ></div>
+  );
+}

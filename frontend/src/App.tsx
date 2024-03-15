3f0fe4dfc866e1ea04832aa52a49d134076a6072
SJ-Kwak <seojin7@ewhain.net>
2024-03-15 20:39:12 +0900

    feat: pwa 프로젝트로 migrate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Map from "./components/map/Map";
 function App() {
   return (
     <div className='app'>
+      <h1>지도</h1>
       <Map />
     </div>
   );
--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function Map() {
   const [map, setMap] = useState();
   useEffect(() => {
-    const mapScript = document.createElement('script');
+    const mapScript = document.createElement("script");
 
     mapScript.async = true;
     mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_KEY}&autoload=false`;
@@ -12,7 +12,7 @@ export default function Map() {
 
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
+        const mapContainer = document.getElementById("map");
         const mapOption = {
           center: new window.kakao.maps.LatLng(37.5635, 126.9467), // 지도의 중심좌표
           level: 4, // 지도의 확대 레벨
@@ -20,22 +20,23 @@ export default function Map() {
         setMap(new window.kakao.maps.Map(mapContainer, mapOption));
       });
     };
-    mapScript.addEventListener('load', onLoadKakaoMap);
+    mapScript.addEventListener("load", onLoadKakaoMap);
   }, []);
 
-  // 지도에 선을 표시한다 
-  var polyline = new window.kakao.maps.Polyline({
-    map: map, // 선을 표시할 지도 객체 
-    path: [ // 선을 구성하는 좌표 배열
-      new window.kakao.maps.LatLng(37.5635, 126.9480),
-      new window.kakao.maps.LatLng(37.563, 126.9476),
-      new window.kakao.maps.LatLng(37.5648, 126.9467)
-    ],
-    strokeWeight: 3, // 선의 두께
-    strokeColor: '#FF0000', // 선 색
-    strokeOpacity: 0.9, // 선 투명도
-    strokeStyle: 'solid' // 선 스타일
-  });
+  // // 지도에 선을 표시한다
+  // var polyline = new window.kakao.maps.Polyline({
+  //   map: map, // 선을 표시할 지도 객체
+  //   path: [
+  //     // 선을 구성하는 좌표 배열
+  //     new window.kakao.maps.LatLng(37.5635, 126.948),
+  //     new window.kakao.maps.LatLng(37.563, 126.9476),
+  //     new window.kakao.maps.LatLng(37.5648, 126.9467),
+  //   ],
+  //   strokeWeight: 3, // 선의 두께
+  //   strokeColor: "#FF0000", // 선 색
+  //   strokeOpacity: 0.9, // 선 투명도
+  //   strokeStyle: "solid", // 선 스타일
+  // });
 
   return (
     <div

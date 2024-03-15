1ec2e8beca54b4e02260db8bd5e1bb698ba2a466
SJ-Kwak <seojin7@ewhain.net>
2024-03-15 22:07:16 +0900

    feat: 마커, 길 표시

--- frontend/src/components/map/KakaoMap.tsx
+++ frontend/src/components/map/KakaoMap.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef, MutableRefObject } from "react";
 import Locations from "./Locations";
+import { positions } from "../../data/Positions";
 
 export default function KakaoMap() {
   const mapRef = useRef<HTMLElement | null>(null);
@@ -8,13 +9,18 @@ export default function KakaoMap() {
   const initMap = () => {
     if (typeof location != 'string' ){
       const container = document.getElementById('map');
+      const locPosition = new kakao.maps.LatLng(location.latitude, location.longitude);
       const options = {
-        center: new kakao.maps.LatLng(location.latitude, location.longitude),
-        level: 4
+        center: locPosition,
+        level: 3
       };
   
       var map = new kakao.maps.Map(container as HTMLElement, options);
       (mapRef as MutableRefObject<any>).current = map;
+      const message = '현위치'
+      currentLocation({ map, locPosition, message });
+      showMarkers({map});
+      showPath({map})
     }
   };
 
@@ -22,9 +28,55 @@ export default function KakaoMap() {
     kakao.maps.load(() => initMap());
   }, [mapRef, location]);
 
+  const currentLocation = ({ map, locPosition, message }: { map: any; locPosition: any; message: string; }) => {
+    var marker = new kakao.maps.Marker({  
+      map: map, 
+      position: locPosition
+    }); 
+    var iwContent = message, // 인포윈도우에 표시할 내용
+      iwRemoveable = true;
+    var infowindow = new kakao.maps.InfoWindow({
+      content : iwContent,
+      removable : iwRemoveable
+    });
+    infowindow.open(map, marker);
+    map.setCenter(locPosition);   
+  }
+
+  var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+    
+  const showMarkers = ({ map }: { map: any }) => {
+    for (var i = 0; i < positions.length; i ++) {
+      var imageSize = new kakao.maps.Size(24, 35);  
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: positions[i].latlng, // 좌표
+        title : positions[i].name, // 마커 타이틀. 마우스 올리면 표시.
+        image : markerImage // 마커 이미지 
+      });
+    }  
+  }
+
+  const tmp = [positions[0].latlng, positions[1].latlng];
+  const showPath = ({ map }: { map: any }) => {
+    var polyline = new kakao.maps.Polyline({
+      path: tmp, // 좌표배열
+      strokeWeight: 5, // 선 두께
+      strokeColor: '#FFAE00', // 선 색깔
+      strokeOpacity: 0.7, // 선 불투명도. 1~0. 0에 가까울수록 투명.
+      strokeStyle: 'solid' // 선 스타일
+    });
+    polyline.setMap(map);
+  }
+
   return (
     <>
       <div id='map' style={{ width: "100%", height: "90vh" }}></div>
+      <button
+        style={{ position:"relative", zIndex:"2"}}
+        onClick={() => initMap()}
+      >현위치로</button>
     </>
   );
 }
---
+++ frontend/src/data/Positions.tsx
@@ -0,0 +1,10 @@
+export const positions = [
+  {
+    name: "대강당",
+    latlng: new kakao.maps.LatLng(37.561085, 126.945120),
+  },
+  {
+    name: "후문",
+    latlng: new kakao.maps.LatLng(37.56353, 126.944546),
+  },
+];
